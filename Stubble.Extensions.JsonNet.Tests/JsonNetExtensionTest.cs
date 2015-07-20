4185313b5b1b0c0bf98e400c07ba40187b39d09c
Romanx <mcauliffe.alex@gmail.com>
2015-07-20 13:09:18 +0100

    Added ValueGetter for JObjects and tests

---
+++ Stubble.Extensions.JsonNet.Tests/JsonNetExtensionTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Stubble.Core;
+using Stubble.Extensions.JsonNet;
+using Xunit;
+
+namespace Stubble.Extensions.JsonNet.Tests
+{
+    public class JsonNetExtensionTest
+    {
+        [Fact]
+        public void It_Can_Get_Values_From_JTokens()
+        {
+            const string json = "{ foo: \"bar\" }";
+
+            var stubble = new StubbleBuilder()
+                                .AddJsonNet()
+                                .Build();
+
+            var obj = JsonConvert.DeserializeObject(json);
+
+            var output = stubble.Render("{{foo}}", obj);
+            Assert.Equal("bar", output);
+        }
+
+        [Fact]
+        public void It_Doesnt_Throw_When_No_Value_Exists()
+        {
+            const string json = "{ foo: \"bar\" }";
+
+            var stubble = new StubbleBuilder()
+                                .AddJsonNet()
+                                .Build();
+
+            var obj = JsonConvert.DeserializeObject(json);
+
+            var output = stubble.Render("{{foo2}}", obj);
+            Assert.Equal("", output);
+        }
+
+        public void Tokens_Return_Correct_Type()
+        {
+        }
+    }
+}
--- Stubble.Extensions.JsonNet/JsonNet.cs
+++ Stubble.Extensions.JsonNet/JsonNet.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Stubble.Core.Interfaces;
 
 namespace Stubble.Extensions.JsonNet
 {
-    public class JsonNet
+    public static class JsonNet
     {
+        public static IStubbleBuilder AddJsonNet(this IStubbleBuilder builder)
+        {
+            return builder
+                .AddValueGetter(typeof (JObject), (value, key) =>
+                {
+                    var token = (JObject)value;
+                    var jValue = (JValue)token[key];
+
+                    return jValue != null ? jValue.Value : null;
+                });
+        }
     }
 }
